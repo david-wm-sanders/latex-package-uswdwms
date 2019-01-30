6fc96d30d0bce360b030d22bc1e846928b5f14ce
David Sanders <david.wm.sanders@gmail.com>
2019-01-30 22:52:57 +0000

    Add updates from the first pentest report

--- make_example.py
+++ make_example.py
@@ -2,9 +2,9 @@ import subprocess
 from pathlib import Path
 
 print(f"Making example...")
-commands = [["pdflatex.exe", "example.tex"],
+commands = [["pdflatex.exe", "-shell-escape", "example.tex"],
             ["bibtex.exe", "example.aux"],
-            ["pdflatex.exe", "example.tex"],
-            ["pdflatex.exe", "example.tex"]]
+            ["pdflatex.exe", "-shell-escape", "example.tex"],
+            ["pdflatex.exe", "-shell-escape", "example.tex"]]
 for command in commands:
     subprocess.run(command)
